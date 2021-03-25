c1e8658b698d1de3e8fe78f371b96c22553f2572
github9582816659 <github9582816659@gmail.com>
2021-03-25 22:40:21 +0530

    update

--- api/src/test/java/com/elearning/ElearningApplicationTests.java
+++ api/src/test/java/com/elearning/ElearningApplicationTests.java
@@ -1,10 +1,26 @@
 package com.elearning;
 
+import io.hypersistence.optimizer.HypersistenceOptimizer;
+import io.hypersistence.optimizer.core.config.JpaConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+
 @SpringBootTest
 class ElearningApplicationTests {
+
+	@PersistenceContext
+	private EntityManagerFactory entityManagerFactory;
+
+	@PostConstruct
+	public void init() {
+		new HypersistenceOptimizer(
+				new JpaConfig(entityManagerFactory)
+		);
+	}
 
 	@Test
 	void contextLoads() {
